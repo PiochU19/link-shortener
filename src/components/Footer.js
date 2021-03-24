dbecfe7d4cf42b92df0d58c28f7ac13d80515aec
PiochU19 <792954018@wp.pl>
2021-03-24 01:03:57 +0100

    Final commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,12 @@ import boat from '../assets/img/boat.png';
 const Footer = () => {
 	return (
 		<div className='Footer'>
-			<h1 className='FooterText'>© Dominik Pioś 2021</h1>
-			<img className='BoatImg' src={boat} />
+			<div className='FooterText'>
+				<h1>© Dominik Pioś 2021</h1>
+			</div>
+			<div className='BoatImg'>
+				<img src={boat} />
+			</div>
 		</div>
 	);
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,8 @@ const Main = () => {
 
 	const [value, setValue] = useState("");
 	const [short_url, set_short_url] = useState("");
-	const [a_short_url, set_a_short_url] = useState("");
+	const [err_msg, set_error] = useState("");
+	const [copied, set_copied] = useState("");
 
 	const handleChange = e => {
 		setValue(e.target.value);
@@ -25,18 +26,43 @@ const Main = () => {
       		const response = await request.json();
 
       		e.target.reset();
+      		setValue("");
 
-      		set_short_url('localhost:8000/'+ response.short_url);
-      		set_a_short_url('http://localhost:8000/'+ response.short_url);
+      		const stat = request.status;
 
-    	} catch (err) {
-      		console.log(err);
+      		if (stat == 201) {
+				set_short_url('localhost:8000/'+ response.short_url);
+				set_copied("");
+      		} else if (stat == 400) {
+      			set_short_url("");
+      			set_copied("");
+      			set_error('Enter valid URL');
+      		}
+
+    	} catch (error) {
+    		e.target.reset();
+    		set_short_url("");
+    		set_copied("");
+      		set_error('Something went wrong with our servers');
     	}	
+  	}
+
+  	const copy_to_clipboard = () => {
+  		const e1 = document.createElement('textarea');
+  		e1.value = short_url;
+  		e1.setAttribute('readonly', '');
+  		e1.style = {position: 'absolute', left: '-9999px'};
+  		document.body.appendChild(e1);
+  		e1.select();
+  		document.execCommand("copy");
+  		document.body.removeChild(e1);
+  		set_copied(true);
   	}
 
 	return (
 		<div className='Main'>
-			{ short_url && <div className='Short-url'> <a href={a_short_url}>{short_url}</a> </div> }
+
+			{ short_url ? <div className='Short-url'>{ copied ? <span className='Link-below'>Copied!</span> : <span className='Link-below'>Click link below to copy</span> } <br /><a href="#" onClick={copy_to_clipboard}>{short_url}</a></div> : <div className='Short-url'>{err_msg}</div> }
 			<div className='Form__group Field'>
 				<form onSubmit={handleSubmit}>
 					<input className='Form__field' type='text' id='url' placeholder='URL' onChange={handleChange} />
