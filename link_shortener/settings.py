0de5702d5b3cb27a6280e08ea29bd6e2374c328f
PiochU19 <792954018@wp.pl>
2021-03-24 01:07:47 +0100

    CORS anonymous throttle rate changed to 10 per day

--- link_shortener/settings.py
+++ link_shortener/settings.py
@@ -83,7 +83,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.SessionAuthentication',),
     'DEFAULT_THROTTLE_CLASSES': ('rest_framework.throttling.AnonRateThrottle',),
-    'DEFAULT_THROTTLE_RATES': {'anon': '1000000/day'},
+    'DEFAULT_THROTTLE_RATES': {'anon': '10/day'},
 }
 
 
